635212cc623fcfde427ebffdf947648e80c6edb2
menglong <meda4299@colorado.edu>
2019-12-11 11:42:41 +0800

    first

---
+++ gotut3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func add(x, y float64) float64 {
+	return x + y
+}
+func main() {
+	num1, num2 := 5.6, 9.5
+	fmt.Println(add(num1, num2))
+	fmt.Println(mutiple("c", "d"))
+
+}
+func mutiple(a, b string) (string, string) {
+	return a, b
+}
